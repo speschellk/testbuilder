e8ed77036b4a834156496239c3071aa29930e809
Kay Christensen <kaychristensen@gmail.com>
2016-11-25 02:20:52 -0500

    Added Discover and Maestro, altered tests

--- detectNetwork.js
+++ detectNetwork.js
@@ -12,29 +12,30 @@
 // The American Express network always starts with a 34 or 37 and is 15 digits long
 // Visa always has a prefix of 4 and a length of 13, 16, or 19.
 // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+// Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
+// Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
 
 var networks = {
-  "Diner's Club": {length: 14, prefix: ["38", "39"]},
-  "American Express": {length: 15, prefix: ["34", "37"]},
-  "Visa": {length: [13, 16, 19], prefix: "4"},
-  "MasterCard": {length: 16, prefix: ["51", "52", "53", "54", "55"]}
+  "Diner's Club":     {length: [14],         prefix: ["38", "39"]},
+  "American Express": {length: [15],         prefix: ["34", "37"]},
+  "Visa":             {length: [13, 16, 19], prefix: ["4"]},
+  "MasterCard":       {length: [16],         prefix: ["51", "52", "53", "54", "55"]},
+  "Discover":         {length: [16, 19],     prefix: ["6011", "644-649", "65"]},
+  "Maestro":          {length: [12, 13, 14, 15, 16, 17, 18, 19], prefix: ["5018", "5020", "5038", "6304"]}
 };
 
 var detectNetwork = function(cardNumber) {
-  var len = cardNumber.length;
-  var pre = cardNumber.slice(0,2);
-  if (pre[0] === "4") {
-    pre = pre[0];
-  };
-
   for (var key in networks) {
     var length = networks[key].length;
     var prefix = networks[key].prefix;
 
-    if ((prefix === pre || prefix.includes(pre)) &&
-    (length === len || length.includes(len) != "undefined" && length.includes(len))) {
-      return key;
+    if (length.includes(cardNumber.length)) {
+      for (var i = 0; i < prefix.length; i++) {
+        if (cardNumber.startsWith(prefix[i])) {
+          return key;
+        };
+      };
     };
   };
-  return "We don't recognize this credit network";
+  return "Not a recognized credit network";
 };
